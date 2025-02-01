58480b9bd45cb660b7ea8155b2228ef236738e63
Anthony Kiranga <kirangaantony@gmail.com>
2025-02-01 12:49:54 +0300

    Parameters

--- Learning_C.c
+++ Learning_C.c
@@ -1,45 +1,50 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+ void birthday(char name[], int age)
+{
+   printf("\nHappy birthday dear %s!", name);
+   printf("\nYou are %d years old!",  age); 
+    
+} 
 
 int main(){ 
+   // logical operators == && (AND) checks if two conditions are true
 
-   char op;
-   double num1;
-   double num2;
-   double result;
+/*float temp = 25;
+bool sunny = true;
 
-   printf("\nEnter an op (+ - * /): ");
-   scanf(" %c", &op);
+if(temp >= 0 && temp <= 30 && sunny){
+    printf("The weather is good!");
+}
+else{
+   printf("The weather is bad!");
+}*/
+   //logical operators = || (OR) checks if at least one condition is true
+   /* float temp = -1000;
 
-   printf("Enter number 1: ");
-   scanf("%lf", &num1);
+    if(temp <= 0 || temp >= 30){
+        printf("\nThe weather is bad!");        
+    } 
+   else{
+      printf("\nThe weather is good!");
+   }*/
+  // logical operators = ! (NOT) reverses the state of a condition
+  
+   /*bool sunny = false;
 
-   printf("Enter number 2: ");
-   scanf("%lf", &num2);
+    if(!sunny ){
+        printf("\nIt's cloudy outside!");
+    }
+    else{
+      printf("\nIt's sunny outside!");
+    }*/
+   char name[] = "Bro";
+   int age = 21;
 
-   switch(op){
-      case '+':
-         result = num1 + num2;
-         printf("\nresult: %.2lf", result);
-         break;
-      case '-':
-         result = num1 - num2;
-         printf("\nresult: %.2lf", result);
-         break;
-      case '*':
-         result = num1 * num2;
-         printf("\nresult: %.2lf", result);
-         break;
-       case '/':
-         result = num1 / num2;
-         printf("\nresult: %.2lf", result);
-         break;
-      default:
-         printf("%c is not valid", op);
-
-   } 
-   
+   birthday(name, age);
+    
    
 
-
- return 0; 
+  return 0; 
  }
