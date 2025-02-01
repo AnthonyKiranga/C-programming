edd608105e96f4649347e263f5b45b4dec8b1b12
Anthony Kiranga <kirangaantony@gmail.com>
2025-02-01 16:11:07 +0300

    While

--- Learning_C.c
+++ Learning_C.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+
 int main()
 {
-   for(int i = 10; i >= 1; i-=3)
-   {
-      printf("%d\n", i);
+  char name[25];
 
-   }
- 
+  printf("\nWhat's your name?: ");
+  fgets(name, 25, stdin);
+  name[strlen(name) - 1] = '\0';
+
+  while(strlen(name) == 0)
+  {
+    printf("\nYou did not enter your name");
+    printf("\nWhat's your name?: ");
+    fgets(name, 25, stdin);
+    name[strlen(name) - 1] = '\0';
+  }
+
+  printf("Hello %s", name);
  
  }
 
@@ -18,4 +28,5 @@ void hello(char name[], int age)
 {
 printf("\nHello %s", name);
 printf("\nYou are %d years old", age);
+
 }
