160b3808e68fd909e047bf402d974a71839049c3
Anthony Kiranga <kirangaantony@gmail.com>
2025-02-03 16:47:42 +0300

    quiz game

--- Learning_C.c
+++ Learning_C.c
@@ -1,43 +1,54 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <ctype.h>
 
 int main()
 { 
-   const int MIN = 1;
-   const int MAX = 100;
-   int guess;
-   int guesses;
-   int answer;
+  char questions[][100] = {"1. What year did the C language debut?: ", 
+                           "2. Who is credited with creating C?:",
+                           "3. What is the predecessor of c?: "};
 
+  char options[][100] = {"A. 1969", "B. 1972", "C. 1975", "D. 1999",
+                         "A. Denmark Ritchie", "B. Nikola Tesla", "C. John Carmack", "D. Doc Brown",
+                         "A. Objective C", "B. B", "C. C++", "D. c#"};  
   
-   srand(time(0));
+   char answers[3] = {'B', 'A', 'B'}; 
+   int numberOfQuestions = sizeof(questions)/sizeof(questions[0]);
 
-   answer = (rand() % MAX) + MIN;
+   char guess;                  
+   int score = 0;
+
+   printf("QUIZ GAME\n");
+
+   for(int i = 0; i < numberOfQuestions; i++)
+   {
+     printf("***********************\n");
+      printf("%s\n", questions[i]);
+     printf("***********************\n"); 
+   for(int j = (i * 4); j < (i * 4) + 4; j++)
+   {
+      printf("%s\n", options[j]);
+   }
 
 
-  do{
-     printf("Enter a guess: ");
-     scanf("%d", &guess);
-     if(guess > answer)
-     {
-        printf("Too high!\n");
-     }
-     else if(guess < answer)
-     {
-      printf("Too low!\n");
-     }
-     else{
-        printf("CORRECT!\n");
-     }
-     guesses++;
-  }while(guess != answer);
-
+      printf("guess: ");
+      scanf(" %c", &guess);
+      //scanf("%c");// clear \n fro input buffer
+      
+      guess = toupper(guess);
+      
+      if(guess == answers[i])
+      {
+         printf("CORRECT!\n");
+         score++;
+      }
+      else
+      {
+         printf("WRONG!\n");
+      }
+   }
   printf("***********************\n");
-  printf("answer: %d\n", answer);
-  printf("guesses: %d\n", guesses);
-  printf("***********************");
-
+  printf("FINAL SCORE: %d/%d\n", score, numberOfQuestions);
+  printf("***********************\n");
   return 0;
 }
   
